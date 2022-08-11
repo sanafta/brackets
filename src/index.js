f4e02b99cb0d1c513bd291f8c82b6f2ab0f4a31b
Ekaterina Kornienko <sanafta.2011@gmail.com>
2022-08-11 19:47:20 +0300

    task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+	let error = false;
+	if (str.length !== 0 && bracketsConfig.length !== 0 && !(str.length % 2)) {
+		for (let i = 0; i < bracketsConfig.length; i++) {
+			if (Array.from(str.matchAll(new RegExp(`\\${bracketsConfig[i][0]}`, 'g'))).length !== Array.from(str.matchAll(new RegExp(`\\${bracketsConfig[i][1]}`, 'g'))).length) {
+				error = true;
+				console.log('pair ERROR');
+				break;
+			}
+		}
+		const regArr = bracketsConfig.map(el => new RegExp(`${/\d/.test(el[0]) ? '' : '\\'}${el[0]}${/\d/.test(el[0]) ? '' : '\\'}${el[1]}`, 'g'));
+		let temp = '';
+		for (let i = 0; i < regArr.length; i++) {
+			temp = str.replaceAll(regArr[i], '');
+			if (str.length !== temp.length) {
+				str = temp;
+				i = 0;
+				i--;
+			}
+		}
+		if (str.length !== 0) {
+			error = true;
+		}
+	} else {
+		error = true;
+	}
+	return !error;
 }
